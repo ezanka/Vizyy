c5b0082429af59a68534083ad7408a52b14577f6
ezanka <stethan345@gmail.com>
2026-01-17 16:12:31 +0100

    init(auth): add prisma and betterauth

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+    },
+    secret: process.env.BETTER_AUTH_SECRET as string,
+    baseUrl: process.env.BETTER_AUTH_URL as string,
+});
